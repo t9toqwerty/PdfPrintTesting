f48e62fda37102eaf6d4773ed9bfca60e6616eb9
Rahul K Jha <jharahulkumar12@gmail.com>
2019-06-27 00:35:58 +0530

    Dynamic HTML via Erb.new

--- app/controllers/print_pdf_controller.rb
+++ app/controllers/print_pdf_controller.rb
@@ -2,7 +2,10 @@
 
 class PrintPdfController < ApplicationController
   def print
-    website_path = ' file:////home/ggku3sys114/RubymineProjects/PdfPrintTesting/index.html'
+    @ids = [1, 2, 3, 4, 5, 6, 7, 8]
+    template_file_path = ERB.new(File.read(Rails.root.to_s + '/app/views/index.html.erb')).result(binding)
+    File.open(Rails.root.to_s + '/tmp/index.html', 'w') {|file| file.write(template_file_path)}
+    website_path = 'file:////' + Rails.root.to_s + '/tmp/index.html'
     tmp = Tempfile.new("pdf-chrome-puppeteer")
     system("node qr_pdf.js #{Shellwords.escape(website_path)} #{Shellwords.escape(tmp.path)}")
     pdf_data = File.read(tmp.path)
